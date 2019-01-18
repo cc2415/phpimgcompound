14cd34f0fcb4117e7286b73dba03cb9713a21932
cc2415 <18819209916@163.com>
2019-01-18 15:52:26 +0800

    1.0.0 初始化

---
+++ src/Ford/Fiesta/Fiesta2016.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/1/18
+ * Time: 15:49
+ */
+
+namespace Ford\Fiesta;
+
+
+class Fiesta2016
+{
+    public static function inof0()
+    {
+        echo 'This is Ford Escape2016';
+    }
+}
